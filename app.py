40a8e4747ce90c832a6c07f99da642b0fa7d0067
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-02-20 17:35:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Test de ML para ligando-receptor")
 
-smile = st.input("Ingresa tu molécula en código SMILES: ", "c1cccc(NC2=O)c1[C@]23[C@@H]4c5n([C@H](C3)C(=O)N4)c(=O)c6c(n5)cccc6")
-df = pd.DataFrame({'smiles': [smile]})
-st.show(df)
+st.title("Test de ML para ligando-receptor")
 
