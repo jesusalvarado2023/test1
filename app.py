4d759241afd3a94ee1bd0dc12b45e34f17a79ebd
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-02-20 17:55:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import streamlit as st
-
+import streamlit.components.v1 as components
+from stmol import showmol
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
 
 st.title("Test de ML para ligando-receptor")
 
+compound_smiles=st.text_input('Ingresa tu código SMILES','FCCC(=O)[O-]')
+m = Chem.MolFromSmiles(compound_smiles)
+
+Draw.MolToFile(m,'mol.png')
+st.image('mol.png')
